2f390df7e02b466a6d3edc1c94e07fb0bbfdcc82
HJ-Jeddens <159947784+HJ-Jeddens@users.noreply.github.com>
2024-02-20 17:26:20 +0100

    Update primeNumbers.js

--- oefening6/scripts/primeNumbers.js
+++ oefening6/scripts/primeNumbers.js
@@ -5,7 +5,7 @@ while (isNaN(input)) input = Number(prompt("Enter a number"));
 
 for (let number = 2; number < input; number++) {
   let isPrime = true;
-  for (let dividend = 2; dividend <= number / 2; dividend++) {
+  for (let dividend = 2; dividend <= Math.sqrt(number); dividend++) {
     if (number % dividend === 0) {
       isPrime = false;
       break;
